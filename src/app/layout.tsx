7c3eacf290a2d995e8d7d351194bef13187238a2
Divyanshu <dnd.mein@gmail.com>
2025-12-08 19:14:02 +0530

    add post creation functionality with toast notifications

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -45,12 +46,15 @@ export default function RootLayout({
                 {/* conatiner to center the content */}
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                   <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
-                    <div className="hidden lg:block lg:col-span-3"><Sidebar/></div>
+                    <div className="hidden lg:block lg:col-span-3">
+                      <Sidebar />
+                    </div>
+                    <div className="lg:col-span-9">{children}</div>
                   </div>
-                  <div className="lg:col-span-9">{children}</div>
                 </div>
               </main>
             </div>
+            <Toaster />
           </ThemeProvider>
         </body>
       </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
-import ModeToggle from "@/components/ModeToggle";
-import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import { syncUser } from "@/actions/user.action";
+import { getDbUserId } from "@/actions/user.action";
+import CreatePost from "@/components/CreatePost";
+import { currentUser } from "@clerk/nextjs/server";
 
 export default async function Home() {
-  await syncUser();
-  
+  const user = await currentUser();
+  const dbUserId = await getDbUserId();
+
   return (
-    <div>
-  <h1>HomePage content</h1>
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="lg:col-span-6">
+        {user ? <CreatePost /> : null}
+
+        <div className="space-y-6">
+          {/* {posts.map((post) => (
+            <PostCard key={post.id} post={post} dbUserId={dbUserId} />
+          ))} */}
+        </div>
+      </div>
+
+      <div className="hidden lg:block lg:col-span-4 sticky top-20">
+      </div>
     </div>
   );
 }
