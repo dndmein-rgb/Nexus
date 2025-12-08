713aa71febec8b894d99974952fd8bacfd06b8fb
Divyanshu <dnd.mein@gmail.com>
2025-12-08 16:52:25 +0530

    Add PostgreSQL adapter and user synchronization functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import ModeToggle from "@/components/ModeToggle";
 import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { syncUser } from "@/actions/user.action";
 
-export default function Home() {
+export default async function Home() {
+  await syncUser();
+  
   return (
     <div>
   <h1>HomePage content</h1>
