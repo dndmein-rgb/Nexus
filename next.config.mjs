9ba1eee0071cb257213e7030311a2ada52e68000
Divyanshu <dnd.mein@gmail.com>
2025-12-11 01:12:06 +0530

    Add UploadThing integration for image uploads in posts

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  transpilePackages: ["@uploadthing/react", "@uploadthing/shared"],
+};
 
 export default nextConfig;
